52e4184c73808961554a11fd1c0097a905c674c0
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-05-25 19:41:39 +0530

    op-file

--- terraform/aws/modules/output-file/main.tf
+++ terraform/aws/modules/output-file/main.tf
@@ -4,21 +4,22 @@ locals {
 
 resource "local_sensitive_file" "global_cloud_values_yaml" {
   content = templatefile("${path.module}/global-cloud-values.yaml.tfpl", {
-    env                        = var.env,
-    environment                = var.environment,
-    building_block             = var.building_block,
-    aws_s3_bucket_name         = var.bucket_name,
-    aws_s3_private_path        = var.s3_private_path,
-    aws_s3_public_path         = var.s3_public_path,
-    aws_s3_dial_state_path     = var.s3_dial_state_path,
-    private_ingressgateway_ip  = var.private_ingressgateway_ip,
-    encryption_string          = var.encryption_string,
-    random_string              = var.random_string,
-    aws_access_key_id          = var.aws_access_key_id,
-    aws_secret_access_key      = var.aws_secret_access_key,
-    aws_region                 = var.aws_region,
-    aws_s3_public_bucket_name  = var.aws_s3_public_bucket_name,
-    aws_s3_private_bucket_name = var.aws_s3_private_bucket_name
+    env                           = var.env,
+    environment                   = var.environment,
+    building_block                = var.building_block,
+    aws_s3_bucket_name            = var.bucket_name,
+    aws_s3_private_path           = var.s3_private_path,
+    aws_s3_public_path            = var.s3_public_path,
+    aws_s3_dial_state_path        = var.s3_dial_state_path,
+    private_ingressgateway_ip     = var.private_ingressgateway_ip,
+    encryption_string             = var.encryption_string,
+    random_string                 = var.random_string,
+    aws_access_key_id             = var.aws_access_key_id,
+    aws_secret_access_key         = var.aws_secret_access_key,
+    aws_region                    = var.aws_region,
+    aws_s3_public_bucket_name     = var.aws_s3_public_bucket_name,
+    aws_s3_private_bucket_name    = var.aws_s3_private_bucket_name,
+    aws_s3_dial_state_bucket_name = var.aws_s3_dial_state_bucket_name
   })
   filename = local.global_values_cloud_file
 }
