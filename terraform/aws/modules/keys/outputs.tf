f1342aff8a4bdee0c1a20ed70039bd41a7535f1c
ameyjadhavaa <amey.jadhav@alignedautomation.com>
2025-05-25 13:30:39 +0530

    keys

--- terraform/aws/modules/keys/outputs.tf
+++ terraform/aws/modules/keys/outputs.tf
@@ -7,11 +7,3 @@ output "random_string" {
   value     = random_password.generated_string.result
   sensitive = true
 }
-
-output "base_location" {
-  value = var.base_location
-}
-
-output "jwt_script_location" {
-  value = local.jwt_script_location
-}
--- terraform/aws/modules/keys/variables.tf
+++ terraform/aws/modules/keys/variables.tf
@@ -1,30 +1,10 @@
-variable "environment" {
-  type        = string
-  description = "Environment name. Used as a prefix for resources."
-}
-
-variable "building_block" {
-  type        = string
-  description = "Building block name. Used as a prefix for resources."
-}
-
-variable "bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket to upload files to."
-}
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region for S3 operations."
-}
-
-variable "base_location" {
-  type        = string
-  description = "Location of Terraform execution folder."
-}
-
-variable "rsa_keys_count" {
-  type        = number
-  description = "Number of RSA keys to generate."
-  default     = 2
+variable "aws_region" {}
+variable "base_location" {}
+variable "rsa_keys_count" {}
+variable "bucket_name" {}
+variable "environment" {}
+variable "enable_terrahelp" {
+  description = "Whether to enable terrahelp encryption"
+  type        = bool
+  default     = false
 }
